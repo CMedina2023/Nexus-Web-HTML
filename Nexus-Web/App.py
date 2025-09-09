dd77e66a6cf8f2047d3bd230a9df25eb80072a84
CMedina2023 <127905423+CMedina2023@users.noreply.github.com>
2025-09-08 22:33:47 -0600

    Update App.py
    redirect

--- Nexus-Web/App.py
+++ Nexus-Web/App.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, send_file
+from flask import Flask, render_template, request, jsonify, send_file, redirect
 from werkzeug.utils import secure_filename
 import os
 import io
@@ -436,5 +436,6 @@ def preview():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
+
 
 
